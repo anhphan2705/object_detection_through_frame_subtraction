bc961c50210497c103ffd116a10f6120802c1c23
Anh Phan <anhphanvt2705@gmail.com>
2023-08-02 15:26:05 +0700

    init

---
+++ video_feed.py
@@ -0,0 +1,31 @@
+# importing libraries
+import numpy as np
+import cv2
+  
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+  
+# creating object
+# fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
+fgbg = cv2.createBackgroundSubtractorMOG2()
+  
+# capture frames from a camera 
+cap = cv2.VideoCapture("./data/vid_1.mp4")
+while(1):
+    # read frames
+    ret, img = cap.read()
+
+    # apply mask for background subtraction
+    fgmask = fgbg.apply(img)
+            
+    # apply transformation to remove noise
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+      
+    # after removing noise
+    cv2.imshow('GMG', fgmask)
+      
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+  
+cap.release()
+cv2.destroyAllWindows()
